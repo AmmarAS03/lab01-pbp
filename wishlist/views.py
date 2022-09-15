5b40cc3c594a1c2a3acb5cb4aad531338083e002
Ammar Ash shiddiq <ammar@Ammars-MacBook-Pro.local>
2022-09-15 11:51:02 +0700

    oke

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,10 @@
 
 from django.shortcuts import render
 from wishlist.models import ItemWishlist
+from django.http import HttpResponse
+from django.core import serializers
+
+
 # Create your views here.
 def show_wishlist(request):
     data_wishlist_item = ItemWishlist.objects.all()
@@ -9,3 +13,15 @@ def show_wishlist(request):
         'name': 'Ammar Ash Shiddiq'
     }
     return render(request, "wishlist.html", context)
+
+def show_xml(request):
+    data = ItemWishlist.objects.all()
+    return HttpResponse(serializers.serialize('xml', data), content_type="applicatiom/xml")
+
+def show_json(request):
+    data = ItemWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = ItemWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
