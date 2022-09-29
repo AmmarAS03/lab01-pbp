a5e2b4bb9df338dc2a32b9b3014d477704a3ec6e
Ammar Ash shiddiq <ammar@Ammars-MacBook-Pro.local>
2022-09-29 11:39:32 +0700

    Lab 4 DONE

--- wishlist/views.py
+++ wishlist/views.py
@@ -21,6 +21,7 @@ def show_wishlist(request):
     context = {
         'list_item': data_wishlist_item,
         'name': 'Ammar Ash Shiddiq',
+        'user': request.user,
         'last_login': request.COOKIES['last_login']
     }
     return render(request, "wishlist.html", context)
